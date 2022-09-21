88e3b2502ca245dc0dd2bed4ba64ea2eb5fe0ae9
Quincy <Quincy@qq.com>
2022-09-21 16:22:27 +0800

    second commit

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -6,6 +6,7 @@ package com.atguigu;
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello Git !");
+        System.out.println("hello Git!");
+        System.out.println("hello Git2!");
     }
 }
