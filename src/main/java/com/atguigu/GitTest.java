5e07aa2e04987c4855d3132495140f4eda788283
Quincy <Quincy@qq.com>
2022-09-21 16:18:46 +0800

    frist commit

---
+++ src/main/java/com/atguigu/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @author 浔
+ * @date 2022/9/21 16:13
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello Git !");
+    }
+}
